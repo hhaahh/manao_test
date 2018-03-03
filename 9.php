d5a0cdf9b6deb5b6957b183b7a909bb7a626d3dc
Ivan <6olinet@gmail.com>
2018-03-03 15:57:43 +0300

    Функция pow

--- 9.php
+++ 9.php
@@ -5,7 +5,7 @@
  * Автоморфное число равно последним разрядам квадрата этого числа. Например, 5 и 25, 6 и 36, 25 и 625.
  */
 $n = 25;
-$n2 = pow($n, 2);
+$n2 = $n*$n;
 $numb = $n;
 $i=1;
 while(true){
@@ -16,9 +16,16 @@ while(true){
         break;
     }
 }
-if($n2%pow(10,$i) == $n){
+if($n2%myPow(10,$i) == $n){
     echo "Число N является автоморфным";
 }else{
     echo "Число N не является автоморфным";
+}
+function myPow($numb, $stepen){
+   $res=$numb;
+   while(--$stepen){
+       $res *= $numb;
+   }
+   return $res;
 }
 
