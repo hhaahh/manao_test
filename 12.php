2941834c44a72c477b52e0f5ffef2646ddb37d3c
Ivan <6olinet@gmail.com>
2018-03-03 18:04:12 +0300

    12.php добавлен

--- 12.php
+++ 12.php
@@ -1,20 +1,25 @@
 <?php
 
-/* 
+/*
  * 12. Определить, является ли заданное целое число N простым.
  */
 
 $n = 201;
-$res = true;
-for($i=2; $i<($n/2); $i++){
-    if(($n%$i)==0){
-        $res = false;
-        break;
-    }
-}
-if($res){
-    echo 'Число является простым';    
-}else{
+if (prost($n)) {
+    echo 'Число является простым';
+} else {
     echo 'Число не является простым';
 }
+
+function prost($n = 1) {
+    $res = true;
+    for ($i = 2; $i < ($n / 2); $i++) {
+        if (($n % $i) == 0) {
+            $res = false;
+            break;
+        }
+    }
+    return $res;
+}
+
 
