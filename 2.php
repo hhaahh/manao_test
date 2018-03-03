9fd8a4744459124df6ba7e094c18c3dd33d0d206
Ivan <6olinet@gmail.com>
2018-03-03 15:22:36 +0300

    вынесена функция из 2.php в functions.php

--- 2.php
+++ 2.php
@@ -1,21 +1,12 @@
 <?php
 
 //2. Получить  все  четырехзначные  числа,  сумма  цифр  которых  равна заданному числу  n.
+require_once 'functions.php';
 $n = 2;
-$res = 0;
 for ($i = 1000; $i < 10000; $i++) {
     if (summC($i) == $n) {
-        $res++;
+        $res[]=$i;
     }
 }
-echo $res;
+print_r($res);
 
-function summC($n = 1000) {
-    $i = 1;
-    $res = 0;
-    while ($n > 0) {
-        $res += $n % 10;
-        (int) $n /= 10;
-    }
-    return $res;
-}
--- functions.php
+++ functions.php
@@ -47,3 +47,13 @@ function palindrom($n){
     return true;
 }
 
+
+function summC($n = 1000) {
+    $i = 1;
+    $res = 0;
+    while ($n > 0) {
+        $res += $n % 10;
+        (int) $n /= 10;
+    }
+    return $res;
+}
