643b1fef88016ffc36ea9089aa8216dfc23f4750
Ivan <6olinet@gmail.com>
2018-03-05 15:29:06 +0300

    Обновление

--- 26.php
+++ 26.php
@@ -1,12 +1,20 @@
 <?php
-
 /* 
  * 26. Среди натуральных чисел n0 ,n1,...,nm найти число с максимальной суммой делителей.
  */
+
 $m = 1000;
 $n = arr(0,$m);
 $maxSumm = 0;
-
+$chislo = 0;
+for($i=0; $i<$m; $i++){
+    $summDel=summDel($n[$i]);
+    if($summDel > $maxSumm){
+        $maxSumm=$summDel;
+        $chislo = $n[$i];
+    }
+}
+echo "Число - $chislo, максимальная сумма делителей - $maxSumm";
 
 
 function summDel($value) {
