e1143b455a35cfbc922139649d494fc78bb63729
Ivan <6olinet@gmail.com>
2018-03-21 12:44:43 +0300

    Добавлены комментарии oop/index.php

--- oop/index.php
+++ oop/index.php
@@ -3,15 +3,15 @@ $start = microtime(true);
 
 class Mnojestvo {
 
-    public $ishodnArr; // массив со всеми исходными значениями 
-    public $razmer; //размер комбинаций
-    public $ishodnRazmer; //исходный размер
+    public $ishodnArr; // массив со всеми исходными символами
+    public $razmer; //длина получаемой строки
+    public $ishodnRazmer; //исходный размер строки
     public $str; //исходная строка
-    public $last;
-    public $arr;
-    public $result;
-    public $pos;
-    public $m;
+    public $last; //последний ключ массива $arr 
+    public $arr; //массив с ключами образуемой новой строки
+    public $result; //массив со всеми новыми строками
+    public $pos; //ключ массива $arr указывающий позицию на которой мы находимся в момент формирования новой строки
+    public $m; //последний ключ массива $ishodnArr
 
     public function __construct($str = '', $razmer = 0) {
         $this->razmer = $razmer;
@@ -23,6 +23,10 @@ class Mnojestvo {
         $this->arr = range(0, $this->razmer - 1);
     }
 
+    /*
+     * Преобразуем ключи $arr в строку и заносим ее в массив $result
+     */
+
     public function getWord() {
         $this->str = '';
         for ($i = 0; $i < $this->razmer; $i++) {
@@ -30,6 +34,11 @@ class Mnojestvo {
         }
         $this->result[] = $this->str;
     }
+
+    /*
+     * Увеличиваем последний элемент массива $arr исключая совпадения с другими элементами
+     * и заносим все корректные полученные экземпляры массива $arr в массив $result с помощью getWord()
+     */
 
     public function lastElementIterations() {
         $psevdo_arr = array_slice($this->arr, 0, $this->pos);
@@ -41,35 +50,48 @@ class Mnojestvo {
         }
     }
 
+    /*
+     * Ищем в массиве $arr значение которое мы можем увеличить, увеличиваем, 
+     * формируем оставшуюся часть с помощью refresh() и сохраняем.
+     */
+
     public function otherElementIteration() {
         while ($this->pos-- > 0) {
             $psevdo_arr = array_slice($this->arr, 0, $this->pos);
             if ($this->arr[$this->pos] ++ < $this->m) {
                 if (in_array($this->arr[$this->pos], $psevdo_arr)) {
                     $this->pos++;
-                    continue;
+                } else {
+                    break;
                 }
-                break;
             } elseif ($this->pos == 0) {
                 return false;
             }
         }
-        $this->refresh($this->pos + 1);
+        $this->refresh();
         $this->getWord();
         return true;
     }
 
+    /*
+     * Получаем все варианты строк указанной длины из исходного набора символов 
+     */
+
     public function getWords() {
         while ($this->otherElementIteration()) {
             $this->pos = $this->last;
-            $this->lastElementsIterations();
+            $this->lastElementIterations();
         }
         return $this->result;
     }
 
-    public function refresh($do) {
+    /*
+     * Формирует часть $arr (всё что больше $pos) минимальными значениями
+     */
+
+    public function refresh() {
         $count = count($this->arr);
-        for ($i = $do; $i < $count; $i++) {
+        for ($i = $this->pos + 1; $i < $count; $i++) {
             $i1 = 0;
             $this->arr[$i] = 0;
             while (in_array($i1, $arr_slice = array_slice($this->arr, 0, $i))) {
@@ -83,10 +105,18 @@ class Mnojestvo {
         return $this->arr;
     }
 
+    /*
+     * Количество комбинаций
+     */
+
     public function combCount($razmerMnoj, $razmerPodmnoj) {
         $res = $this->factorial($razmerMnoj) / $this->factorial($razmerMnoj - $razmerPodmnoj);
         return $res;
     }
+
+    /*
+     * Факториал числа
+     */
 
     private function factorial($value) {
         if ($value == 0) {
@@ -111,6 +141,6 @@ $razmerItog = $mnojestvoModel->combCount($mnojestvoModel->ishodnRazmer, $mnojest
     Строка: <?= $n ?>(N)
     Длина итоговой строки: <?= $m ?>(M)
     <?php print_r($mnojestvoModel->getWords()); ?>
-    Память: <?= (memory_get_usage() - $memory) / 1024 / 1024 ?>
-    <?= $time = microtime(true) - $start ?>
+    Задействовано памяти: <?= ((memory_get_usage() - $memory) / 1024) / 1024 ?> Мб
+    Время выполнения: <?= $time = microtime(true) - $start ?> сек
 </pre>
